82cb9b1ce2d9a82a3061d48d4224609bc9316ce1
David EVAN <devan.exa@gmail.com>
2023-02-17 13:33:20 +0100

    feature/sample-data

--- src/main/java/com/medhead/ers/bsns_ems/application/data/sample/DataSampleLoader.java
+++ src/main/java/com/medhead/ers/bsns_ems/application/data/sample/DataSampleLoader.java
@@ -1,15 +1,29 @@
 package com.medhead.ers.bsns_ems.application.data.sample;
 
+import com.medhead.ers.bsns_ems.application.data.repository.EmergencyRepository;
+import com.medhead.ers.bsns_ems.domain.entity.Emergency;
+import com.medhead.ers.bsns_ems.domain.valueObject.GPSCoordinates;
+import com.medhead.ers.bsns_ems.domain.valueObject.MedicalSpeciality;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.UUID;
+
 @Configuration
 public class DataSampleLoader {
     @Bean
-    CommandLineRunner initDatabase() {
-        return args -> {};
+    CommandLineRunner initDatabase(EmergencyRepository emergencyRepository) {
+        Emergency emergency = Emergency.builder()
+                .description("Test Emergency")
+                .gpsCoordinates(GPSCoordinates.builder()
+                        .latitude(50.51746719004866)
+                        .longitude(-0.05958983237771111).build())
+                .medicalSpeciality(MedicalSpeciality.CARDIOLOGY)
+                .patientId(UUID.randomUUID().toString())
+                .build();
+        return args -> {
+            emergencyRepository.save(emergency);
+        };
     }
-
-
 }
