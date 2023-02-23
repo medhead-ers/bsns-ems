11e5d8af9f255b74fe94ac2d89f96c1b09b9aa5d
David EVAN <devan.exa@gmail.com>
2023-02-23 09:48:40 +0100

    feature/publish-message-when-emergency-created

--- src/main/java/com/medhead/ers/bsns_ems/domain/service/implementation/EmergencyServiceImpl.java
+++ src/main/java/com/medhead/ers/bsns_ems/domain/service/implementation/EmergencyServiceImpl.java
@@ -1,6 +1,9 @@
 package com.medhead.ers.bsns_ems.domain.service.implementation;
 
 import com.medhead.ers.bsns_ems.application.data.repository.EmergencyRepository;
+import com.medhead.ers.bsns_ems.application.messaging.exception.MessagePublicationFailException;
+import com.medhead.ers.bsns_ems.application.messaging.message.factory.MessageFactory;
+import com.medhead.ers.bsns_ems.application.messaging.service.definition.MessagePublisher;
 import com.medhead.ers.bsns_ems.domain.entity.Emergency;
 import com.medhead.ers.bsns_ems.domain.exception.EmergencyNotFoundException;
 import com.medhead.ers.bsns_ems.domain.service.definition.EmergencyService;
@@ -14,9 +17,18 @@ import java.util.UUID;
 public class EmergencyServiceImpl implements EmergencyService {
     @Autowired
     EmergencyRepository emergencyRepository;
+    @Autowired
+    MessagePublisher messagePublisher;
+
     @Override
-    public Emergency saveEmergency(Emergency emergency){
-            return emergencyRepository.save(emergency);
+    public Emergency saveEmergency(Emergency emergency) {
+            Emergency emergencyCreated = emergencyRepository.save(emergency);
+            try {
+                messagePublisher.publish(MessageFactory.createEmergencyCreatedMessage(emergencyCreated));
+                return emergencyCreated;
+            }
+            // Pour le POC, aucune gestion d'erreur sur la publication de message ne sera implémentée.
+            catch (MessagePublicationFailException e){return emergencyCreated;}
     }
 
     @Override
